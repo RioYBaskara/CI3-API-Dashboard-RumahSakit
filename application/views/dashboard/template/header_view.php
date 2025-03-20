c2268df256f14b622fe97072443846211ebe86e8
Rio Yudha Baskara <156874101+RioYBaskara@users.noreply.github.com>
2025-03-20 23:49:28 +0700

    Toggle dark light mode

--- application/views/dashboard/template/header_view.php
+++ application/views/dashboard/template/header_view.php
@@ -66,11 +66,42 @@
             right: 20px;
             z-index: 1000;
         }
+
+        /* toggle dark/light */
+        .toggle-light-dark {
+            position: fixed;
+            bottom: 40px;
+            right: 200px;
+            z-index: 1000;
+        }
     </style>
 </head>
 
 <body class=" layout-fluid">
     <script src="<?= base_url() ?>public/assets/tabler/dist/js/demo-theme.min.js?1692870487"></script>
+
+    <!-- Dark/Light Mode -->
+    <div class="toggle-light-dark">
+        <a href="?theme=dark" class="px-0 nav-link hide-theme-dark" title="Enable dark mode" data-bs-toggle="tooltip"
+            data-bs-placement="bottom">
+            <!-- Download SVG icon from http://tabler-icons.io/i/moon -->
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24"
+                stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                <path d="M12 3c.132 0 .263 0 .393 0a7.5 7.5 0 0 0 7.92 12.446a9 9 0 1 1 -8.313 -12.454z" />
+            </svg>
+        </a>
+        <a href="?theme=light" class="px-0 nav-link hide-theme-light" title="Enable light mode" data-bs-toggle="tooltip"
+            data-bs-placement="bottom">
+            <!-- Download SVG icon from http://tabler-icons.io/i/sun -->
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24"
+                stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                <path d="M12 12m-4 0a4 4 0 1 0 8 0a4 4 0 1 0 -8 0" />
+                <path d="M3 12h1m8 -9v1m8 8h1m-9 8v1m-6.4 -15.4l.7 .7m12.1 -.7l-.7 .7m0 11.4l.7 .7m-12.1 -.7l-.7 .7" />
+            </svg>
+        </a>
+    </div>
 
     <!-- Refresh -->
     <div class="fixed-button-Refresh">
